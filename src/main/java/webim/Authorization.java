ab5036f570bf5b51d553a0633d24fee5079dd894
Dmitry Petrov <dmitrypetrov93@gmail.com>
2019-02-07 21:17:42 +0300

    Version 0.048

--- src/main/java/webim/Authorization.java
+++ src/main/java/webim/Authorization.java
@@ -19,6 +19,7 @@ import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.UserAuthResponse;
 import com.vk.api.sdk.objects.friends.responses.GetResponse;
+import com.vk.api.sdk.objects.users.UserFull;
 import com.vk.api.sdk.objects.users.UserXtrCounters;
 import com.vk.api.sdk.queries.friends.FriendsGetMutualOrder;
 import com.vk.api.sdk.queries.friends.FriendsGetOrder;
@@ -45,84 +46,70 @@ public class Authorization extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        
+
         HttpSession session = request.getSession();
         String code = (String) session.getAttribute("code");
-        
+
         int APP_ID = 6843248;
         String CLIENT_SECRET = "4ZOc9BOTt8FBUonu0jxe";
         String REDIRECT_URI = "https://webim-test1.herokuapp.com/listener";
-      
-        TransportClient transportClient = HttpTransportClient.getInstance(); 
+
+        TransportClient transportClient = HttpTransportClient.getInstance();
         VkApiClient vk = new VkApiClient(transportClient);
         UserAuthResponse authResponse = null;
         try {
-            authResponse = vk.oauth() 
-                    .userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET, REDIRECT_URI, code) 
-                    .execute();
+            authResponse = vk.oauth().userAuthorizationCodeFlow(APP_ID,
+                    CLIENT_SECRET, REDIRECT_URI, code).execute();
         } catch (ApiException e) {
             e.printStackTrace();
         } catch (ClientException e) {
             e.printStackTrace();
-        } 
-        UserActor actor = new UserActor(authResponse.getUserId(), authResponse.getAccessToken());
+        }
+        UserActor actor = new UserActor(authResponse.getUserId(),
+                authResponse.getAccessToken());
         session.setAttribute("actor", actor.toString());
-        
-        
+
         try {
-            GetResponse getResponse = vk.friends().get(actor)
-                    .userId(actor.getId())
-                    .count(10)
-                    .order(FriendsGetOrder.HINTS)
-                    .execute();
-            
+            GetResponse getResponse =
+                    vk.friends().get(actor).userId(actor.getId()).count(10)
+                            .order(FriendsGetOrder.HINTS).execute();
+
             List<Integer> friends = getResponse.getItems();
             if (friends == null) {
                 return;
             }
-            
-            
 
-            List<UserXtrCounters> userInfo;
-            
             List<UserField> fields = new ArrayList<UserField>();
             fields.add(UserField.PHOTO_200);
             fields.add(UserField.DOMAIN);
 
+            List<UserXtrCounters> userInfo;
+            for (int i = 0; i < friends.size(); i++) {
 
-            
-            
-            for(int i = 0; i < friends.size(); i++) {
-                
                 String id = "" + friends.get(i);
-                
-                userInfo = vk.users().get(actor)
-                        .userIds(id)
-                        .fields(fields)
-                        .execute();
-                
-                String userInfoStr = "";
-                
-                for(int j = 0; j < userInfo.size(); j++) {
-                    userInfoStr += userInfo.get(j) + " ";
-                }
-                
-                session.setAttribute("friend" + i, userInfoStr);      
-            }
-            
 
-            
+                userInfo = vk.users().get(actor).userIds(id).fields(fields)
+                        .execute();
+
+                String userInfoStr = "";
+
+                for (int j = 0; j < userInfo.size(); j++) {
+                    UserFull userFull = (UserFull) userInfo.get(i);
+                    userInfoStr = userFull.getFirstName() + " "
+                            + userFull.getLastName() + " "
+                            + userFull.getDomain() + " "
+                            + userFull.getPhoto200();
+                }
+
+                session.setAttribute("friend" + i, userInfoStr);
+            }
+
         } catch (ApiException e) {
             e.printStackTrace();
         } catch (ClientException e) {
             e.printStackTrace();
-        } 
-        
-        
+        }
 
-        
-        
-        
         response.sendRedirect("/OAuthCode");
     }
 
