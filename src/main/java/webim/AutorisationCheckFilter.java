09c7cef7e0bc45b0ea8814d7d210bef4e7c07512
Dmitry Petrov <dmitrypetrov93@gmail.com>
2019-02-11 19:32:45 +0300

    Added AutorisationCheckFilter.java to project

---
+++ src/main/java/webim/AutorisationCheckFilter.java
@@ -0,0 +1,59 @@
+
+package webim;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AutorisationCheckFilter
+ */
+public class AutorisationCheckFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public AutorisationCheckFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpSession session = req.getSession(true);
+        
+        if(session.isNew()) {
+            resp.sendRedirect("/index.html");
+            //chain.doFilter(request, response);
+        } else {
+            resp.sendRedirect("/RequestFriends");
+        }
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+}
